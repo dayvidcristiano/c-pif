788e652a6b027e15673ca6190d344b41fa625629
Dayvid Cristiano <dayvidcristianosilva@gmail.com>
2026-08-26 16:49:08 -0300

    Atualização da Lista 01

---
+++ Lista-Cap1/exercicio09.c
@@ -0,0 +1,11 @@
+
+#include <stdio.h> 
+#include <stdlib.h> 
+ 
+int main() 
+{ 
+    printf("%c%c%cPrimeiro programa", '\n', '\t', '\"'); 
+    printf("%c", "\""); 
+    system("PAUSE"); 
+    return 0; 
+} 
